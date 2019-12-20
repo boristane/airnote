fc6f82a1852f8f7d1c55205ae1b5a5ed2a778dc9
Boris Tane <boris.tane@gmail.com>
2019-12-20 17:23:30 +0000

    wip

--- lib/screens/intro.dart
+++ lib/screens/intro.dart
@@ -32,7 +32,7 @@ class Intro extends StatelessWidget {
 }
 
 class IntroSlide extends StatefulWidget {
-  List _data = [];
+  List _data;
 
   IntroSlide(this._data);
   @override
@@ -45,7 +45,7 @@ class _IntroSlideState extends State<IntroSlide> {
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
-    final height = screenHeight < 500 ? 460 : screenHeight * 0.7;
+    final cardHeight = screenHeight < 500 ? 460 : screenHeight * 0.7;
 
     final Container dots = Container(
       width: widget._data.length.toDouble() * 20,
@@ -53,17 +53,88 @@ class _IntroSlideState extends State<IntroSlide> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         mainAxisSize: MainAxisSize.min,
-        children: List.generate(widget._data.length,
-            (index) {
+        children: List.generate(widget._data.length, (index) {
           return AirnoteDotIndicator(active: _currentIndex == index);
         }),
       ),
     );
 
     return Container(
-      child: dots,
+      child: Column(
+        children: <Widget>[
+          // PageView.builder(
+          //   itemCount: widget._data.length,
+          //   onPageChanged: (int position) {
+          //     setState(() {
+          //       _currentIndex = position;
+          //     });
+          //   },
+          //   itemBuilder: (BuildContext context, int position) {
+          //     // final currentSlide = widget._data[position];
+          //     // return IntroSlideCard(cardHeight, currentSlide);
+          //     return Container();
+          //   },
+          // ),
+          IntroSlideCard(cardHeight, widget._data[0]),
+          dots,
+        ],
+      ),
     );
   }
 }
 
+class IntroSlideCard extends StatelessWidget {
+  double _height;
+  SlideInfo _currentSlide;
 
+  IntroSlideCard(this._height, this._currentSlide);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: _height,
+      child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+        Container(),
+        Container(
+          width: 270,
+          height: 380,
+          margin: EdgeInsets.only(top: 100.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20.0),
+            boxShadow: [
+              BoxShadow(
+                  color: Color(0xFF3C4858).withOpacity(.4),
+                  offset: Offset(6.0, 10.0),
+                  blurRadius: 20.0),
+            ],
+            image: DecorationImage(
+              image: AssetImage(
+                _currentSlide.image,
+              ),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Align(
+            alignment: Alignment(0.1, 1.0),
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              height: 100,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                color: Colors.blueGrey.withOpacity(0.8),
+              ),
+              child: Text(
+                _currentSlide.title,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        )
+      ]),
+    );
+  }
+}
