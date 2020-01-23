cc4f917d6df6fa0189d23e1c5fc5ef4fee662b1f
Boris Tane <boris.tane@gmail.com>
2020-01-23 12:47:20 +0000

    Refactor

--- lib/components/circular-button.dart
+++ lib/components/circular-button.dart
@@ -6,26 +6,33 @@ class AirnoteCircularButton extends StatelessWidget {
   final Function onTap;
   final bool isLarge;
 
-  AirnoteCircularButton({Key key, this.icon, this.onTap, this.isLarge = false}) : super(key: key);
+  AirnoteCircularButton({Key key, this.icon, this.onTap, this.isLarge = false})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return InkResponse(
-      onTap: this.onTap,
-      child: Container(
-        padding: EdgeInsets.all(isLarge ? 15 : 10),
-        margin: EdgeInsets.only(bottom: 10),
-        decoration: BoxDecoration(
-          color: AirnoteColors.white,
-          border: Border.all(color: Colors.black12),
-          borderRadius: BorderRadius.circular(100),
-          boxShadow: [
-            BoxShadow(
-                color: AirnoteColors.primary.withOpacity(.5),
-                offset: Offset(1.0, 10.0),
-                blurRadius: 10.0),
-          ],
+    return Container(
+      margin: EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+        color: AirnoteColors.white,
+        border: Border.all(color: Colors.black12),
+        borderRadius: BorderRadius.circular(18),
+        boxShadow: [
+          BoxShadow(
+              color: AirnoteColors.primary.withOpacity(.5),
+              offset: Offset(1.0, 10.0),
+              blurRadius: 10.0),
+        ],
+      ),
+      child: Material(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0),),
+        child: InkWell(
+          customBorder: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0),),
+          onTap: this.onTap,
+          child: Container(
+            padding: EdgeInsets.all(isLarge ? 15 : 10),
+            child: icon,
+          ),
         ),
-        child: icon,
       ),
     );
   }
