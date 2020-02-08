09eb72549afc1442967ebbff4343d1e1b024f3f8
Boris Tane <boris.tane@gmail.com>
2020-02-08 12:23:07 +0000

    Makes it kinda work

--- lib/views/create-entry.dart
+++ lib/views/create-entry.dart
@@ -9,21 +9,6 @@ class CreateEntry extends StatefulWidget {
   _CreateEntryState createState() => _CreateEntryState();
 }
 
-const languages = const [
-  const Language('Francais', 'fr_FR'),
-  const Language('English', 'en_US'),
-  const Language('Pусский', 'ru_RU'),
-  const Language('Italiano', 'it_IT'),
-  const Language('Español', 'es_ES'),
-];
-
-class Language {
-  final String name;
-  final String code;
-
-  const Language(this.name, this.code);
-}
-
 class _CreateEntryState extends State<CreateEntry> {
   SpeechRecognition _speechRecognition;
 
@@ -31,26 +16,18 @@ class _CreateEntryState extends State<CreateEntry> {
   bool _isListening = false;
 
   String _text = '';
-
-  //String _currentLocale = 'en_US';
-  Language selectedLang = languages.first;
-
   @override
   initState() {
     super.initState();
-    activateSpeechRecognizer();
+    _activateSpeechRecognizer();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  void activateSpeechRecognizer() {
-    print('_MyAppState.activateSpeechRecognizer... ');
+  void _activateSpeechRecognizer() {
     _speechRecognition = new SpeechRecognition();
     _speechRecognition.setAvailabilityHandler(onSpeechAvailability);
-    _speechRecognition.setCurrentLocaleHandler(onCurrentLocale);
     _speechRecognition.setRecognitionStartedHandler(onRecognitionStarted);
     _speechRecognition.setRecognitionResultHandler(onRecognitionResult);
     _speechRecognition.setRecognitionCompleteHandler(onRecognitionComplete);
-    // _speech.setErrorHandler(errorHandler);
     _speechRecognition
         .activate()
         .then((res) => setState(() => _isAvailable = res));
@@ -60,8 +37,8 @@ class _CreateEntryState extends State<CreateEntry> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-          child: ListView(
-      children: <Widget>[
+      child: ListView(
+        children: <Widget>[
           Container(child: Text("Title")),
           Container(
             padding: EdgeInsets.fromLTRB(20, 10, 20, 20),
@@ -75,55 +52,61 @@ class _CreateEntryState extends State<CreateEntry> {
             ),
           ),
           Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                AirnoteCircularButton(
-                  icon: Icon(Icons.cancel),
-                  onTap: _isListening ? () => cancel() : null,
-                ),
-                AirnoteCircularButton(
-                  icon: Icon(Icons.mic),
-                  isLarge: true,
-                  onTap: _isAvailable && !_isListening
-                        ? () => start()
-                        : null,
-                ),
-                AirnoteCircularButton(
-                  icon: Icon(Icons.stop),
-                  onTap: _isListening ? () => stop() : null,
-                ),
-              ],
-            ),
-      ],
-    ),
-        ));
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              AirnoteCircularButton(
+                icon: Icon(Icons.cancel),
+                onTap: cancel,
+              ),
+              AirnoteCircularButton(
+                icon: Icon(Icons.mic),
+                isLarge: true,
+                onTap: start,
+              ),
+              AirnoteCircularButton(
+                icon: Icon(Icons.stop),
+                onTap: stop,
+              ),
+            ],
+          ),
+        ],
+      ),
+    ));
   }
 
-  void start() => _speechRecognition
-      .listen(locale: selectedLang.code)
-      .then((result) => print('_MyAppState.start => result $result'));
+  void start() {
+    if (!(_isAvailable && !_isListening)) {
+      return;
+    }
+    _speechRecognition
+        .listen(locale: "en_US")
+        .then((result) => print('_MyAppState.start => result $result'));
+  }
 
-  void cancel() =>
-      _speechRecognition.cancel().then((result) => setState(() => _isListening = result));
+  void cancel() {
+    if (!_isListening) {
+      return;
+    }
+    _speechRecognition
+        .cancel()
+        .then((result) => setState(() => _isListening = result));
+  }
 
-  void stop() => _speechRecognition.stop().then((result) {
-        setState(() => _isListening = result);
-      });
+  void stop() {
+    if (!_isListening) {
+      return;
+    }
+    _speechRecognition.stop().then((result) {
+      setState(() => _isListening = result);
+    });
+  }
 
   void onSpeechAvailability(bool result) =>
       setState(() => _isAvailable = result);
-
-  void onCurrentLocale(String locale) {
-    print('_MyAppState.onCurrentLocale... $locale');
-    setState(
-        () => selectedLang = languages.firstWhere((l) => l.code == locale));
-  }
 
   void onRecognitionStarted() => setState(() => _isListening = true);
 
   void onRecognitionResult(String text) => setState(() => _text = text);
 
   void onRecognitionComplete() => setState(() => _isListening = false);
-
-  void errorHandler() => activateSpeechRecognizer();
 }
