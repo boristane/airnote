f0c54be54f163c8462d5d6dd890d23ea2324aa8f
Boris Tane <boris.tane@gmail.com>
2020-03-20 10:37:36 +0000

    Add back button to entry type

--- lib/views/create-entry/entry-type.dart
+++ lib/views/create-entry/entry-type.dart
@@ -1,4 +1,5 @@
 import 'package:airnote/components/flat-button.dart';
+import 'package:airnote/components/option-button.dart';
 import 'package:airnote/components/raised-button.dart';
 import 'package:airnote/view-models/routine.dart';
 import 'package:airnote/views/create-entry/record.dart';
@@ -17,26 +18,45 @@ class _SelectEntryTypeState extends State<SelectEntryType> {
   Widget build(BuildContext context) {
     final AirnoteRaisedButton _assistedButton = AirnoteRaisedButton(
         text: "Guided entry",
-        onPressed: () =>
-            Navigator.of(context).pushNamed(RoutineView.routeName, arguments: 1));
+        onPressed: () => Navigator.of(context)
+            .pushNamed(RoutineView.routeName, arguments: 1));
     final AirnoteFlatButton _soloButton = AirnoteFlatButton(
         text: "Free flow",
         onPressed: () {
-            final routineViewModel = Provider.of<RoutineViewModel>(context);
-            routineViewModel.reset();
-            Navigator.of(context).pushNamed(RecordEntry.routeName);
+          final routineViewModel = Provider.of<RoutineViewModel>(context);
+          routineViewModel.reset();
+          Navigator.of(context).pushNamed(RecordEntry.routeName);
         });
-    return Align(
-        alignment: Alignment.center,
-              child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _assistedButton,
-            _soloButton,
-          ],
+    return Stack(
+      children: <Widget>[
+        Align(
+          alignment: Alignment.center,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _assistedButton,
+              _soloButton,
+            ],
+          ),
         ),
-      
+        Align(
+          alignment: Alignment.topLeft,
+          child: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.all(15),
+              child: AirnoteOptionButton(
+                icon: Icon(Icons.arrow_downward),
+                onTap: () {
+                  if (Navigator.of(context).canPop()) {
+                    Navigator.of(context).pop();
+                  }
+                },
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
