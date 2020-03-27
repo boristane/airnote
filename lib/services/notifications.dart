6aa6dbd0d40644eb6f3228201606aee474f99ef4
Boris Tane <boris.tane@gmail.com>
2020-03-27 13:38:33 +0000

    make call to save new notif user

--- lib/services/notifications.dart
+++ lib/services/notifications.dart
@@ -1,16 +1,23 @@
 import 'dart:async';
-
+import 'package:airnote/services/api.dart';
 import 'dart:io' show Platform;
 import 'package:airnote/models/user.dart';
+import 'package:airnote/utils/messages.dart';
+import 'package:dio/dio.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 
 class NotificationsService {
   FirebaseMessaging _fcm = FirebaseMessaging();
+  Dio _apiClient;
+  static final String _baseUrl = _getEndpoint();
+  static ApiService _apiService = ApiService(baseUrl: _baseUrl);
 
   StreamSubscription iosSubscription;
 
   NotificationsService() {
-
+    _apiService.clientSetup();
+    this._apiClient = _apiService.client;
   }
 
   void initPushNotifications(User user) {
@@ -41,8 +48,33 @@ class NotificationsService {
     String fcmToken = await _fcm.getToken();
     if (fcmToken != null) {
       final os = Platform.operatingSystem;
-      print("Saving token to database $fcmToken for user ${user.uuid} on $os");
-      // TODO save in backend
+      final requestBody = {
+        "token": fcmToken,
+        "uuid": user.uuid,
+        "os": os,
+        "topic": "quotes"
+      };
+      try {
+        print("Saving token to database $fcmToken for user ${user.uuid} on $os");
+        final response = await _apiClient.post("/user", data: requestBody);
+        return response;
+      } on DioError catch (err) {
+        final data = err.response?.data ?? {};    
+        final message = (data is String || data is ResponseBody) ? AirnoteMessage.unknownError : data["message"] ?? AirnoteMessage.unknownError;
+        print(message);
+      }
+    }
+  }
+
+  static String _getEndpoint() {
+    if (kReleaseMode) {
+      return "http://ec2-3-8-125-65.eu-west-2.compute.amazonaws.com/notifications";
+    } else {
+      if (Platform.isAndroid) {
+        return "http://10.0.2.2:8080/notifications";
+      } else {
+        return "http://localhost:8080/notifications";
+      }
     }
   }
 }
